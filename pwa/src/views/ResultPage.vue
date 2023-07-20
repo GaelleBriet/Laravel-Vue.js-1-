91bf31ca68ac06ff40537ed8f3cee29ea6f3ea66
GaelleBriet <gaelle.briet@protonmail.com>
2023-07-20 22:04:03 +0200

    🚧 PROGRESS:  integration resultpage

---
+++ pwa/src/views/ResultPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TitleComponent from '../components/TitleComponent'
+</script>
+<template>
+    <div class="main-content">
+    <TitleComponent />
+  </div>
+</template>
+<style lang="scss"></style>
