4810da93d599879441f05b7bca840ae796bc7252
GaelleBriet <gaelle.briet@protonmail.com>
2023-07-20 20:43:50 +0200

    🚧 PROGRESS:  integration Form

--- pwa/src/components/Forms/FormComponent.vue
+++ pwa/src/components/Forms/FormComponent.vue
@@ -1,9 +1,16 @@
-<script setup></script>
-<script setup></script>
+<script setup>
+import InputSelectComponent from './InputSelectComponent.vue'
+import InputCheckboxComponent from './InputCheckboxComponent.vue'
+import InputTextComponent from './InputTextComponent.vue'
+</script>
 
 <template>
   <form class="estimator-form" action="result.html">
     <div class="errors">Le nom du projet est obligatoire.</div>
+
+    <InputTextComponent />
+
+    <InputCheckboxComponent />
 
     <div class="input-group">
       <span class="main-label">Développements spécifiques</span>
@@ -14,6 +21,8 @@
         <i class="fa fa-plus"></i>
       </button>
     </div>
+
+    <InputSelectComponent />
 
     <button type="submit" class="button">Obtenir l'estimation</button>
   </form>
