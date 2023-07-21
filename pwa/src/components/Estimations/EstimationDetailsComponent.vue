b2e3fccf7e2dfd662317b3eafe04f2af750c078a
GaelleBriet <gaelle.briet@protonmail.com>
2023-07-21 11:02:04 +0200

    🎨 IMPROVE:  results page

--- pwa/src/components/Estimations/EstimationDetailsComponent.vue
+++ pwa/src/components/Estimations/EstimationDetailsComponent.vue
@@ -1,7 +1,62 @@
 <script setup></script>
 
 <template>
-  <div class="main-content"></div>
+  <div class="main-content">
+    <p class="project-name">Estimations de temps pour le projet : O'clock</p>
+
+    <table class="table-result">
+      <thead>
+        <tr>
+          <th>Développements</th>
+          <th>Temps</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mise en place du projet</td>
+          <td>4h</td>
+        </tr>
+        <tr>
+          <td>Homepage</td>
+          <td>7h</td>
+        </tr>
+        <tr>
+          <td>Blog</td>
+          <td>3h</td>
+        </tr>
+        <tr>
+          <td>Offres d'emplois</td>
+          <td>12h</td>
+        </tr>
+        <tr>
+          <td>Événements</td>
+          <td>14h</td>
+        </tr>
+        <tr>
+          <td>Module météo</td>
+          <td>4h</td>
+        </tr>
+        <tr class="project-infos">
+          <td>Spécificités</td>
+          <td>Temps supplémentaire</td>
+        </tr>
+        <tr>
+          <td>Design : Complexe</td>
+          <td>4h</td>
+        </tr>
+        <tr>
+          <td>Type de projet : Laravel</td>
+          <td>0h</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>40h</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
--- pwa/src/views/ResultPage.vue
+++ pwa/src/views/ResultPage.vue
@@ -1,63 +1,11 @@
 <script setup>
 import TitleComponent from '../components/TitleComponent.vue'
+import EstimationDetailsComponent from '../components/Estimations/EstimationDetailsComponent.vue';
 </script>
 <template>
   <div class="main-content">
     <TitleComponent :title="'Résultat de l\'estimation'" :className="main - title" />
-    <p class="project-name">Estimations de temps pour le projet : O'clock</p>
-
-    <table class="table-result">
-      <thead>
-        <tr>
-          <th>Développements</th>
-          <th>Temps</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Mise en place du projet</td>
-          <td>4h</td>
-        </tr>
-        <tr>
-          <td>Homepage</td>
-          <td>7h</td>
-        </tr>
-        <tr>
-          <td>Blog</td>
-          <td>3h</td>
-        </tr>
-        <tr>
-          <td>Offres d'emplois</td>
-          <td>12h</td>
-        </tr>
-        <tr>
-          <td>Événements</td>
-          <td>14h</td>
-        </tr>
-        <tr>
-          <td>Module météo</td>
-          <td>4h</td>
-        </tr>
-        <tr class="project-infos">
-          <td>Spécificités</td>
-          <td>Temps supplémentaire</td>
-        </tr>
-        <tr>
-          <td>Design : Complexe</td>
-          <td>4h</td>
-        </tr>
-        <tr>
-          <td>Type de projet : Laravel</td>
-          <td>0h</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>Total</td>
-          <td>40h</td> 
-        </tr>
-      </tfoot>
-    </table>
+    <EstimationDetailsComponent />
   </div>
 </template>
 <style lang="scss" scoped></style>
